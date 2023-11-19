6e63f462f50f2c2101687ae37922bd9217c10b74
Seema Rida <121886487+seemarida@users.noreply.github.com>
2023-11-19 15:14:52 -0800

    Update ListExamples.java

--- ListExamples.java
+++ ListExamples.java
@@ -9,41 +9,41 @@ class ListExamples {
   // the StringChecker returns true, and not the elements that return false, in
   // the same order they appeared in the input list;
   static List<String> filter(List<String> list, StringChecker sc) {
-    List<String> result = new ArrayList<>();
+    List<String> myResult = new ArrayList<>();
     for(String s: list) {
       if(sc.checkString(s)) {
-        result.add(0, s);
+        myResult.add(0, s);
       }
     }
-    return result;
+    return myResult;
   }
 
 
   // Takes two sorted list of strings (so "a" appears before "b" and so on),
   // and return a new list that has all the strings in both list in sorted order.
   static List<String> merge(List<String> list1, List<String> list2) {
-    List<String> result = new ArrayList<>();
+    List<String> myResult = new ArrayList<>();
     int index1 = 0, index2 = 0;
     while(index1 < list1.size() && index2 < list2.size()) {
       if(list1.get(index1).compareTo(list2.get(index2)) < 0) {
-        result.add(list1.get(index1));
+        myResult.add(list1.get(index1));
         index1 += 1;
       }
       else {
-        result.add(list2.get(index2));
+        myResult.add(list2.get(index2));
         index2 += 1;
       }
     }
     while(index1 < list1.size()) {
-      result.add(list1.get(index1));
+      myResult.add(list1.get(index1));
       index1 += 1;
     }
     while(index2 < list2.size()) {
-      result.add(list2.get(index2));
+      myResult.add(list2.get(index2));
       // change index1 below to index2 to fix test
       index1 += 1;
     }
-    return result;
+    return myResult;
   }
 
 
